6637eb114881b2cffaafec9d0a77eeb09ef6bb7c
Meletis Papadopoulos <meletiop22@hotmail.com>
2024-08-01 11:31:50 +0300

    Add cleanup file, to remove all test resources.

---
+++ clean-up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Remove Deployment/Service/HPA
+kubectl delete -f php-apache.yaml --force --grace-period 0
+
+printf "\n"
+
+# Uninstall Gatekeeper Resources
+kubectl delete -f gatekeeper.yaml --force -grace-period 0
+
+printf "\n"
+
+# Delete Constraint & Constraint Template
+kubectl delete -f hpa-replicas-constraint.yaml --force --grace-period 0
+kubectl delete -f hpa-replicas.yaml --force --grace-period 0
+
+printf "\n"
+
+# Remove Metrics Server
+kubectl delete -f metrics-server.yaml --force --grace-period 0
+
+# Check status
+echo $?
