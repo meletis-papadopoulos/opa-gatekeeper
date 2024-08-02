11cc7842acc72ce17f4ceb13ca432e850f97ff9b
meletis-papadopoulos <meletiop22@hotmail.com>
2024-08-02 07:45:38 +0300

    Update increase-load.sh

--- increase-load.sh
+++ increase-load.sh
@@ -2,4 +2,4 @@
 
 # Run this in a separate terminal
 # so that the load generation continues and you can carry on with the rest of the steps
-kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://php-apache > /dev/null; done"
+kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://php-apache; done"
